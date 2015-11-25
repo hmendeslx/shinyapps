5504de8e9cc545db4b2161535cc31e914e81e69f
Helder Mendes <hmendes@tap.pt>
2015-11-25 10:20:09 +0000

    initial commit

---
+++ server.R
@@ -0,0 +1,28 @@
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  output$myname <- renderText(input$name)
+  output$myage <- renderText(input$age)
+  output$mygender <- renderText(input$gender)
+  
+  
+  
+#   output$distPlot <- renderPlot({
+# 
+#     # generate bins based on input$bins from ui.R
+#     x    <- faithful[, 2]
+#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+# 
+#     # draw the histogram with the specified number of bins
+#     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# 
+#   })
+
+})
