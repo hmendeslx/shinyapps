f428e7c208d39dfb7a48f10844aa9dbc02c11d20
Helder Mendes <hmendes@tap.pt>
2016-01-28 09:03:14 +0000

    ction on histogram plotting

--- app6/server.R
+++ app6/server.R
@@ -11,10 +11,10 @@ shinyServer(function(input, output) {
 
   output$myhist <- renderPlot({
     colm <- as.numeric(input$var)
-    hist(iris[,colm], breaks = seq(0, max(iris[,colm], l=input$bins+1)), col=input$color,
+    hist(iris[,colm], breaks = seq(0, max( iris[,colm]), l=input$bins+1 ), col=input$color,
          main="Histogram of Iris Dataset", xlab=names(iris[colm]))
     
   })
-  
+   
     
 })
